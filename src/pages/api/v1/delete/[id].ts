daec3fe7841a483bd17344c9b5269d97b8c3802e
Glowman554 <vossjanick62@gmail.com>
2024-12-21 15:16:17 +0100

    added delete endpoint

---
+++ src/pages/api/v1/delete/[id].ts
@@ -0,0 +1,31 @@
+import type { APIContext } from 'astro';
+import { and, eq } from 'drizzle-orm';
+import { Files } from '../../../../database/schema';
+import { db } from '../../../../database/database';
+import { getProject } from '../../../../actions/projects';
+
+const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET',
+    'Access-Control-Allow-Headers': 'Content-Type, Authentication',
+};
+
+export async function GET(context: APIContext) {
+    const { id } = context.params;
+
+    const token = context.request.headers.get('Authentication');
+    if (!token) {
+        throw new Error('Missing authentication token');
+    }
+    const project = await getProject(token);
+
+    await db.delete(Files).where(and(eq(Files.id, id!), eq(Files.project, project.id)));
+
+    return new Response('{}', {
+        status: 200,
+        headers: {
+            'Content-Type': 'application/json',
+            ...headers,
+        },
+    });
+}
